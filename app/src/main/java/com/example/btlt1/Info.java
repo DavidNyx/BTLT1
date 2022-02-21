861facbf850d99f1502f401a080e1897ea193ac7
DavidNyx <david.quoccuong2001@gmail.com>
2022-02-22 06:09:30 +0700

    done

--- app/src/main/java/com/example/btlt1/Info.java
+++ app/src/main/java/com/example/btlt1/Info.java
@@ -1,6 +1,42 @@
 package com.example.btlt1;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Info extends AppCompatActivity {
+public class Info extends AppCompatActivity implements View.OnClickListener{
+    Button button4;
+    TextView tx1, tx2, tx3, tx4, tx5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+        button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+        String user = getIntent().getStringExtra("user");
+        String pw = getIntent().getStringExtra("pw");
+        String birth = getIntent().getStringExtra("birth");
+        String gen = getIntent().getStringExtra("gen");
+        String hob = getIntent().getStringExtra("hob");
+        tx1 = findViewById(R.id.textView14);
+        tx2 = findViewById(R.id.textView16);
+        tx3 = findViewById(R.id.textView18);
+        tx4 = findViewById(R.id.textView21);
+        tx5 = findViewById(R.id.textView23);
+        tx1.setText(user);
+        tx2.setText(pw);
+        tx3.setText(birth);
+        tx4.setText(gen);
+        tx5.setText(hob);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v.getId() == button4.getId())
+            this.finishAffinity();
+    }
 }
--- app/src/main/java/com/example/btlt1/MainActivity.java
+++ app/src/main/java/com/example/btlt1/MainActivity.java
@@ -3,24 +3,100 @@ package com.example.btlt1;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button button2;
+    Button b1, b2, b3;
+    EditText ed1, ed2, ed3, ed4;
+    RadioButton r1, r2;
+    CheckBox c1, c2, c3;
+    DatePickerDialog datePickerDialog;
+    Calendar cal = Calendar.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button2 = findViewById(R.id.button2);
-        button2.setOnClickListener(this);
+        b1 = findViewById(R.id.button);
+        b2 = findViewById(R.id.button2);
+        b3 = findViewById((R.id.button3));
+        ed1 = findViewById(R.id.editTextTextPersonName);
+        ed2 = findViewById(R.id.editTextTextPassword);
+        ed3 = findViewById(R.id.editTextTextPassword2);
+        ed4 = findViewById(R.id.editTextDate);
+        r1 = findViewById(R.id.radioButton12);
+        r2 = findViewById(R.id.radioButton13);
+        c1 = findViewById(R.id.checkBox);
+        c2 = findViewById(R.id.checkBox2);
+        c3 = findViewById(R.id.checkBox3);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+        b3.setOnClickListener(this);
+        DatePickerDialog.OnDateSetListener dateSetListener  = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                ed4.setText(date);
+            }
+        };
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        datePickerDialog = new DatePickerDialog(this, dateSetListener, year, month, day);
     }
 
     @Override
     public void onClick(View v){
-        if(v.getId() == button2.getId())
+        if(v.getId() == b1.getId())
+            datePickerDialog.show();
+        if(v.getId() == b2.getId())
             startActivity(new Intent(MainActivity.this, MainActivity.class));
+        if(v.getId() == b3.getId()) {
+            if(ed2.getText().toString().equals(ed3.getText().toString())) {
+                Intent intentMainInfo = new Intent(MainActivity.this, Info.class);
+                intentMainInfo.putExtra("user", ed1.getText().toString());
+                intentMainInfo.putExtra("pw", ed2.getText().toString());
+                intentMainInfo.putExtra("birth", ed4.getText().toString());
+                if(r1.isChecked())
+                    intentMainInfo.putExtra("gen", "Male");
+                if(r2.isChecked())
+                    intentMainInfo.putExtra("gen", "Female");
+                String hob = "";
+                if(c1.isChecked())
+                    hob += "Tennis";
+                if(c2.isChecked()){
+                    if(hob.equals(""))
+                        hob += "Futbal";
+                    else
+                        hob += ", Futbal";
+                }
+                if(c3.isChecked()){
+                    if(hob.equals(""))
+                        hob += "Others";
+                    else
+                        hob += ", Others";
+                }
+                intentMainInfo.putExtra("hob", hob);
+                startActivity((intentMainInfo));
+            }
+        }
+    }
+
+    private String makeDateString(int day, int month, int year){
+        return day + "/" + month + "/" + year;
     }
 }
